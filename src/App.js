029bcccf4805dd5d2069c12aaa26351b57f14a53
Henrique Aguiar <sos.lhti@gmail.com>
2021-09-15 07:43:04 -0300

    Seção Blog-posts concluída

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Header from './components/Header/Header';
 import Headline from './components/Headline/Headline';
 import Sobre from './components/Sobre/Sobre';
 import Terapias from './components/Terapias/Terapias';
+import BlogPosts from './components/BlogPosts/BlogPosts';
 
 import GlobalStyles from './global/GlobalStyles';
 import './global/custom-font-awesome.css';
-
-
 
 function App() {
   return (
@@ -18,6 +17,7 @@ function App() {
 			<Headline />
 			<Sobre />
 			<Terapias />
+			<BlogPosts />
 		</>
   );
 }
