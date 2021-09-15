a41f164273812f15ede7cd3694a7e34c71fa614f
Henrique Aguiar <sos.lhti@gmail.com>
2021-09-15 09:06:55 -0300

    Final before build

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Headline from './components/Headline/Headline';
 import Sobre from './components/Sobre/Sobre';
 import Terapias from './components/Terapias/Terapias';
 import BlogPosts from './components/BlogPosts/BlogPosts';
+import Contato from './components/Contato/Contato';
+import Footer from './components/Footer/Footer';
 
 import GlobalStyles from './global/GlobalStyles';
 import './global/custom-font-awesome.css';
@@ -18,6 +20,8 @@ function App() {
 			<Sobre />
 			<Terapias />
 			<BlogPosts />
+			<Contato />
+			<Footer />
 		</>
   );
 }
